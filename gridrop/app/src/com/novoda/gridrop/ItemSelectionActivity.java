8aa7f5647a4408e0775a46edba8563ae18984c22
Andreas Stütz <andreas.stuetz@gmail.com>
2013-01-26 16:46:09 +0100

    more changes on ItemSelectionActivity

--- gridrop/app/src/com/novoda/gridrop/ItemSelectionActivity.java
+++ gridrop/app/src/com/novoda/gridrop/ItemSelectionActivity.java
@@ -1,23 +1,40 @@
 package com.novoda.gridrop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.novoda.gridrop.ui.adapters.ItemSelectionPagerAdapter;
 
 public class ItemSelectionActivity extends SherlockFragmentActivity {
 
-	private ViewPager mPager;
+	private ViewPager pager;
+	private ItemSelectionPagerAdapter adapter;
 
-	// private
+	private List<Integer> selectedLayouts = new ArrayList<Integer>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_item_selection);
 
-		mPager = (ViewPager) findViewById(R.id.activity_item_selection_pager);
+		pager = (ViewPager) findViewById(R.id.activity_item_selection_pager);
 
+		adapter = new ItemSelectionPagerAdapter(getSupportFragmentManager());
+		pager.setAdapter(adapter);
+	}
+
+	public void addLayoutId(Integer resId) {
+		if (!selectedLayouts.contains(resId)) {
+			selectedLayouts.add(resId);
+		}
+	}
+
+	public void removeLayoutId(Integer resId) {
+		selectedLayouts.remove(resId);
 	}
 
 }
