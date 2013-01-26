ef5831cc06b4a9c585af4190132d8c9065b81394
Andreas Stütz <andreas.stuetz@gmail.com>
2013-01-26 17:41:10 +0100

    styled ActionBar

--- gridrop/app/src/com/novoda/gridrop/ItemSelectionActivity.java
+++ gridrop/app/src/com/novoda/gridrop/ItemSelectionActivity.java
@@ -8,46 +8,61 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.novoda.gridrop.ui.adapters.ItemSelectionPagerAdapter;
 
 public class ItemSelectionActivity extends SherlockFragmentActivity {
 
-	private ViewPager pager;
-	private ItemSelectionPagerAdapter adapter;
+    private ViewPager pager;
+    private ItemSelectionPagerAdapter adapter;
 
-	private ArrayList<Integer> selectedLayouts = new ArrayList<Integer>();
+    private ArrayList<Integer> selectedLayouts = new ArrayList<Integer>();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_item_selection);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_selection);
 
-		pager = (ViewPager) findViewById(R.id.activity_item_selection_pager);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		adapter = new ItemSelectionPagerAdapter(getSupportFragmentManager());
-		pager.setAdapter(adapter);
-	}
+        pager = (ViewPager) findViewById(R.id.activity_item_selection_pager);
 
-	public void addLayoutId(Integer resId) {
-		if (!selectedLayouts.contains(resId)) {
-			selectedLayouts.add(resId);
-		}
-	}
+        adapter = new ItemSelectionPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+    }
 
-	public void removeLayoutId(Integer resId) {
-		selectedLayouts.remove(resId);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent i = new Intent(ItemSelectionActivity.this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
-	public void onCancelClicked(View view) {
-		setResult(RESULT_CANCELED);
-		finish();
-	}
+    public void addLayoutId(Integer resId) {
+        if (!selectedLayouts.contains(resId)) {
+            selectedLayouts.add(resId);
+        }
+    }
 
-	public void onInsertClicked(View view) {
-		Intent returnIntent = new Intent();
-		returnIntent.putIntegerArrayListExtra(MainActivity.EXTRA_LAYOUT_IDS, selectedLayouts);
-		setResult(RESULT_OK, returnIntent);
-		finish();
-	}
+    public void removeLayoutId(Integer resId) {
+        selectedLayouts.remove(resId);
+    }
+
+    public void onCancelClicked(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    public void onInsertClicked(View view) {
+        Intent returnIntent = new Intent();
+        returnIntent.putIntegerArrayListExtra(MainActivity.EXTRA_LAYOUT_IDS, selectedLayouts);
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
 
 }
